fc9b592c1f39be298c70d52e8a0766e0cf032bbd
mnutt346 <mnutt346@gmail.com>
2018-11-12 17:38:44 -0600

    (doc) timeBetweenSteps not being passed in to setTimeout

--- src/init.js
+++ src/init.js
@@ -15,10 +15,10 @@ $(document).ready(function() {
      * A new object of the given type will be created and added
      * to the stage.
      */
-    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+    let makeBlinkyDancer = $(this).data('makeBlinkyDancer');
 
     // get the maker function for the kind of dancer we're supposed to make
-    var dancerMakerFunction = window[dancerMakerFunctionName];
+    var dancerMakerFunction = window[makeBlinkyDancer];
 
     // make a dancer with a random position
 
